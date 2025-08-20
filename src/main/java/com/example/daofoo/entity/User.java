42988e91dd07ea4739a3c0cfa813e920a735dc06
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-20 12:16:42 -0400

    Creada entidad User

---
+++ src/main/java/com/example/daofoo/entity/User.java
@@ -0,0 +1,66 @@
+package com.example.daofoo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long id;
+
+  @Column(name = "user_name")
+  private String name;
+
+  @Column(name = "user_email")
+  private String email;
+
+  @CreationTimestamp
+  private Timestamp createdAt;
+
+  @UpdateTimestamp
+  private Timestamp updatedAt;
+
+  public User() {
+  }
+
+  public User(Long id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
