0a0f0e50a3eb98341ac875aee508cb325d16ac1d
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-20 11:44:03 -0400

    Primer commit

---
+++ src/main/java/com/example/daofoo/DaoFooApplication.java
@@ -0,0 +1,13 @@
+package com.example.daofoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoFooApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DaoFooApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/daofoo/DaoFooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daofoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoFooApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
